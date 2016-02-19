d06e9aa2282fa7a5fca625e40b0e03d5d782bbb5
estarter <estarter@gmail.com>
2016-02-19 21:30:49 +0100

    working on install script

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+CHECK_ZSH_INSTALLED=$(grep /zsh$ /etc/shells | wc -l)
+if [ ! $CHECK_ZSH_INSTALLED -ge 1 ]; then
+    printf "${YELLOW}Zsh is not installed!${NORMAL} Please install zsh first!\n"
+    exit
+fi
+unset CHECK_ZSH_INSTALLED
+
+# If this user's login shell is not already "zsh", attempt to switch.
+TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+    # If this platform provides a "chsh" command (not Cygwin), do it, man!
+    if hash chsh >/dev/null 2>&1; then
+        printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+        chsh -s $(grep /zsh$ /etc/shells | tail -1)
+    # Else, suggest the user do so manually.
+    else
+        printf "I can't change your shell automatically because this system does not have chsh.\n"
+        printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+    fi
+fi
+
 cp -r oh-my-zsh ~/.oh-my-zsh
 rm -rf ~/.oh-my-zsh/.git
 cp -r .git/modules/oh-my-zsh ~/.oh-my-zsh/.git
